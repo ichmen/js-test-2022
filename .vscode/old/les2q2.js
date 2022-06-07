bc13ed9122a9b1823d788899aabb22e1cfaef6d6
ichmen <ichmen@rambler.ru>
2022-06-07 16:54:38 +0300

    directory rebuild

---
+++ .vscode/old/les2q2.js
@@ -0,0 +1,6 @@
+let age=44;
+console.log(age);
+const passportId=444560;
+console.log(passportId);
+var height=180;
+console.log(height);
---
+++ .vscode/old/les3q1.js
@@ -0,0 +1,6 @@
+const id = 3464;
+let name = 'queen';
+let isAvailable = false;
+let price = 66323;
+const color = 'grey';
+let storeAddress = null;
