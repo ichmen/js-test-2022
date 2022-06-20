f9b336e7ec7f614540fcfa8b6dcad9e31d5a0ae8
ichmen <ichmen@rambler.ru>
2022-06-20 18:16:10 +0300

    new

--- task_js_hello/index.js
+++ task_js_hello/index.js
@@ -1,15 +1,34 @@
-function run() {
-  var a = 0;
-
-  if (a == 1) {
-    return c;
-  }
-
-  for (let i = 1; i < 10; i++) {
-    console.log(i);
-  }
-
-  return (d = a);
-}
-
-run();
+/* eslint-disable prefer-const */
+/* eslint-disable prefer-object-spread */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-destructuring */
+const customers = {
+  "customer-id-1": {
+    name: "William",
+    age: 54,
+  },
+  "customer-id-2": {
+    name: "Tom",
+    age: 17,
+  },
+  "customer-id-3": {
+    name: "John",
+    age: 20,
+  },
+};
+const getCustomersList = (obj) => {
+  let newObj = Object.assign({}, obj);
+  // eslint-disable-next-line prefer-const
+  let a = Object.entries(newObj)
+    .map((elem) => {
+      let workObj = { ...elem[1] };
+      workObj.id = elem[0];
+      return workObj;
+    })
+    .flat()
+    .sort((a, b) => a.age - b.age);
+  return a;
+};
+// eslint-disable-next-line no-unused-expressions
+// objEntries.flat(1);
